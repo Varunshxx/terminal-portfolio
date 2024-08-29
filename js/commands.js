7a3fd49a82ed1e574e6a145af2c16eca068f0c8d
Varun Sharma <varunshx@gmail.com>
2024-08-29 19:33:53 +0530

    Complete Terminal Project

---
+++ js/commands.js
@@ -0,0 +1,96 @@
+var twitter = "https://www.twitter.com/Varunshx/";
+var password = "vscode";
+var linkedin = "https://www.linkedin.com/in/varun-sharma/";
+var instagram = "https://www.instagram.com/varushxx/";
+var github = "https://github.com/Varunshxx/";
+var email = 'varunshx@gmail.com';
+var VirtuZone = 'https://landing-page-swart-omega.vercel.app/';
+var ApexGym = 'https://apex-gym-three.vercel.app/';
+var NutShell = 'https://nut-shell.vercel.app/';
+
+whois = [
+  "<br>",
+  "Hey, I'm Varun Sharma!",
+  "I'm a Front-end Developer who builds engaging websites like this one",
+  "After graduating with a Bachelor's in Computer Application,",
+  "I'm just starting to explore the field.",
+  "I'm excited to learn and share my journey as I dive into technology.",
+  "While I don't have professional experience,",
+  "I'm looking forward to growing and connecting with others who are passionate about tech.", 
+  "Stay tuned for updates as I get started!",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of Who am I?â€ is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/varunshx' + '</a>',
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/varun-sharma' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/varunshxx' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/varunshxx' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  '1.VirtuZone        <a href="' + VirtuZone + '" target="_blank">Landing Page' + '</a>',
+  '2.NutShell         <a href="' + NutShell + '" target="_blank">Summarize Articles' + "</a>",
+  '3.ApexGym          <a href="' + ApexGym + '" target="_blank">Go Gym' + '</a>',
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Varun?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">VS Not A Corporation. All right reserved.</span>',
+"<br>",
+"<br>",
+"                     ░██╗░░░░░░░██╗███████╗██╗░░░░░░█████╗░░█████╗░███╗░░░███╗███████╗  ████████╗░█████╗░",
+"                     ░██║░░██╗░░██║██╔════╝██║░░░░░██╔══██╗██╔══██╗████╗░████║██╔════╝  ╚══██╔══╝██╔══██╗",
+"                     ░╚██╗████╗██╔╝█████╗░░██║░░░░░██║░░╚═╝██║░░██║██╔████╔██║█████╗░░  ░░░██║░░░██║░░██║",
+"                     ░░████╔═████║░██╔══╝░░██║░░░░░██║░░██╗██║░░██║██║╚██╔╝██║██╔══╝░░  ░░░██║░░░██║░░██║",
+"                     ░░╚██╔╝░╚██╔╝░███████╗███████╗╚█████╔╝╚█████╔╝██║░╚═╝░██║███████╗  ░░░██║░░░╚█████╔╝",
+"                     ░░░╚═╝░░░╚═╝░░╚══════╝╚══════╝░╚════╝░░╚════╝░╚═╝░░░░░╚═╝╚══════╝  ░░░╚═╝░░░░╚════╝░",
+
+"                                                 ███╗░░░███╗██╗░░░██╗",
+"                                                 ████╗░████║╚██╗░██╔╝",
+"                                                 ██╔████╔██║░╚████╔╝░",
+"                                                 ██║╚██╔╝██║░░╚██╔╝░░",
+"                                                 ██║░╚═╝░██║░░░██║░░░",
+"                                                 ╚═╝░░░░░╚═╝░░░╚═╝░░░",
+
+"                             ██████╗░░█████╗░██████╗░████████╗███████╗░█████╗░██╗░░░░░██╗░█████╗░",
+"                             ██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝██╔════╝██╔══██╗██║░░░░░██║██╔══██╗",
+"                             ██████╔╝██║░░██║██████╔╝░░░██║░░░█████╗░░██║░░██║██║░░░░░██║██║░░██║",
+"                             ██╔═══╝░██║░░██║██╔══██╗░░░██║░░░██╔══╝░░██║░░██║██║░░░░░██║██║░░██║",
+"                             ██║░░░░░╚█████╔╝██║░░██║░░░██║░░░██║░░░░░╚█████╔╝███████╗██║╚█████╔╝",
+"                             ╚═╝░░░░░░╚════╝░╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░░░░░╚════╝░╚══════╝╚═╝░╚════╝░",
+"<br>",
+"<br>",
+ '<span class="color2">Welcome to my interactive web terminal.</span>',
+ "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
